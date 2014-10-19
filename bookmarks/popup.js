0f0a0ef52ab87e8a465f048a0978dac0603d2471
Adrian Lozano <adrian.loma@gmail.com>
2014-10-19 03:08:30 -0500

    simple login form html

--- bookmarks/popup.js
+++ bookmarks/popup.js
@@ -2,6 +2,7 @@
 // chrome.tabs.create({}, function (e){
 //   alert('hey bro');
 // });
+Parse.initialize("zMzw4YOroYkmhM2YUgPjNMc9BQTxelTMwlb3Oy3h", "GCQ4URRqPsoyXyYVlc48rXuqZEr82Yd4DNeU2Bne");
 
 var cionApp = {
   startApp: function() {
@@ -12,5 +13,4 @@ var cionApp = {
 // Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
   cionApp.startApp();
-  alert('asdf');
 });
