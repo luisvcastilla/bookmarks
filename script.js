54bd0f670c70e7dab894c397a67fb205f9fcb8ad
luisvcastilla <luis@sititec.com>
2014-10-18 22:01:08 -0500

    úser.html

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-var cionApp = angular.module('cionApp', ['ngRoute']);			
-cionApp.config(function($routeProvider,$locationProvider) {
-	$locationProvider.html5Mode(true).hashPrefix('!');
-	$routeProvider
-		// route for the home page
+var cionApp = angular.module('cionApp', ['ngRoute','ngAnimate']);			
+cionApp.config(function($routeProvider, $locationProvider) {
+	// $locationProvider.html5Mode(true).hashPrefix('!');
+	$routeProvider		
 		.when('/', {
 			templateUrl : 'pages/inicio.html',
 			controller  : 'mainController'
+		})					
+		.when('/user', {
+			templateUrl : 'pages/user.html',
+			controller  : 'userController'
 		})									
 		.otherwise({ redirectTo: "/" });			
 });
 cionApp.run(function($rootScope, $location) {	   		
 	console.log('running');
 });
+cionApp.controller('userController', function($scope, $routeParams) {									
+	$scope.pageClass = 'page-all';
+	console.log($routeParams);
+});	
 cionApp.controller('mainController', function($scope, $routeParams) {									
 	$scope.pageClass = 'page-all';
 });	
